5b90ef0319849afa158da1110f927769d220dd24
Hyungwon Lee <hueleev@gmail.com>
2022-10-20 14:42:02 +0900

    test2

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+
+npm run build
+cp dist/index.html dist/404.html
+git add dist -f
+git commit -m "Adding dist"
+git subtree push --prefix dist origin gh-pages
--- src/layouts/TheHeader.vue
+++ src/layouts/TheHeader.vue
@@ -1,42 +1,26 @@
 <template>
-	<v-card>
-		<v-layout>
-			<v-navigation-drawer expand-on-hover rail style="border-color: black">
-				<v-list>
-					<v-list-item
-						:prepend-avatar="imgUrl"
-						title="MOZZI"
-						subtitle="ymz@gmailcom"
-					>
-				</v-list-item>
-				</v-list>
+	<v-navigation-drawer expand-on-hover rail app style="border-color: black">
+		<v-list>
+			<v-list-item :prepend-avatar="imgUrl" title="MOZZI" subtitle="ymz@gmailcom">
+			</v-list-item>
+		</v-list>
 
-				<v-divider color="black"></v-divider>
+		<v-divider color="black"></v-divider>
 
-				<v-list density="compact" nav>
-					<v-list-item
-						prepend-icon="mdi-folder"
-						title="Board"
-						:active="$route.path == '/board'"
-						value="myboard"
-						to="board"
-					></v-list-item>
-					<v-list-item
-						prepend-icon="mdi-account-multiple"
-						title="Grooming"
-						value="grooming"
-						to="grooming"
-					></v-list-item>
-					<v-list-item
+		<v-list density="compact" nav>
+			<v-list-item prepend-icon="mdi-folder" title="Board" :active="$route.name == 'board'" value="myboard" to="board">
+			</v-list-item>
+			<v-list-item prepend-icon="mdi-account-multiple" title="Grooming" value="grooming"
+				:active="$route.name == 'grooming'" to="grooming"></v-list-item>
+			<!-- <v-list-item
 						prepend-icon="mdi-star"
 						title="Starred"
 						value="starred"
-					></v-list-item>
-				</v-list>
-			</v-navigation-drawer>
-			<slot></slot>
-		</v-layout>
-	</v-card>
+						aria-disabled="true"
+					></v-list-item> -->
+		</v-list>
+	</v-navigation-drawer>
+	<slot></slot>
 </template>
 
 <script setup>
