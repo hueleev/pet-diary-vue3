4b058962e3452dba0ec5271bd722415451c349fc
Hyungwon Lee <hueleev@gmail.com>
2022-10-21 13:50:25 +0900

    edit router

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import BoardView from '../views/BoardView.vue';
+import GroomingView from '../views/GroomingView.vue';
 
 const router = createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
@@ -16,15 +17,7 @@ const router = createRouter({
 		{
 			path: '/grooming',
 			name: 'grooming',
-			component: () => import('../views/GroomingView.vue'),
-		},
-		{
-			path: '/about',
-			name: 'about',
-			// route level code-splitting
-			// this generates a separate chunk (About.[hash].js) for this route
-			// which is lazy-loaded when the route is visited.
-			component: () => import('../views/AboutView.vue'),
+			component: GroomingView,
 		},
 	],
 });
