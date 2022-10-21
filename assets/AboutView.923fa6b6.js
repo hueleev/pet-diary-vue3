05a44cc1f053715dc077ac5ae0fd7205d58614a1
Hyungwon Lee <hueleev@gmail.com>
2022-10-21 13:50:43 +0900

    Adding dist

--- assets/AboutView.923fa6b6.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./_plugin-vue_export-helper.cdc0426e.js";import{o as t,c as o,e as s}from"./index.770b033c.js";const _={},c={class:"about"},n=s("h1",null,"This is an about page",-1),a=[n];function r(i,u){return t(),o("div",c,a)}const f=e(_,[["render",r]]);export{f as default};
